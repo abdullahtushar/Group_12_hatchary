ec55f349a8349de154ab713f4148520e9901edd8
abdullahtushar <aamtushar09@gmail.com>
2025-04-06 12:29:45 +0600

    testing_1

--- src/main/java/com/example/oop_final_project/AbdullahAlMamun/FarmManager.java
+++ src/main/java/com/example/oop_final_project/AbdullahAlMamun/FarmManager.java
@@ -1,4 +1,9 @@
 package com.example.oop_final_project.AbdullahAlMamun;
 
 public class FarmManager {
-}
+        public static void main(String[] args) {
+            System.out.println("hello farm manager");
+        }
+    }
+
+
