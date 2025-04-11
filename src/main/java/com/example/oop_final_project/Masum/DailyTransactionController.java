430f6decc1ba8aee12ff8e2a0ebd6d04c91778ae
masum179 <2211403@iub.edu.bd>
2025-04-12 04:35:54 +0600

    Add DailyTransaction and DailyTransactionController

---
+++ src/main/java/com/example/oop_final_project/Masum/DailyTransactionController.java
@@ -0,0 +1,44 @@
+package com.example.oop_final_project.Masum;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
+public class DailyTransactionController {
+    @javafx.fxml.FXML
+    private TableColumn sourceOrPurposeColumn;
+    @javafx.fxml.FXML
+    private Label errorLabe;
+    @javafx.fxml.FXML
+    private TextField sourcePurposeTF;
+    @javafx.fxml.FXML
+    private TextField noteTF;
+    @javafx.fxml.FXML
+    private TableColumn incomeOrExpanceColumn;
+    @javafx.fxml.FXML
+    private TableColumn noteColumn;
+    @javafx.fxml.FXML
+    private Text dailyTransactionTable;
+    @javafx.fxml.FXML
+    private DatePicker addDP;
+    @javafx.fxml.FXML
+    private TableColumn amountColumn;
+    @javafx.fxml.FXML
+    private TableColumn dateColumn;
+    @javafx.fxml.FXML
+    private TextField amountTF;
+    @javafx.fxml.FXML
+    private ComboBox incomeOrExpanceCB;
+
+    @javafx.fxml.FXML
+    public void backOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void logoutOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void addTransactionOnAction(ActionEvent actionEvent) {
+    }
+}
