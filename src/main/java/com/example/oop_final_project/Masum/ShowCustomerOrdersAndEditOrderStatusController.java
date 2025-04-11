81833aae34974bf48aeca6dacf15479de2e260f0
masum179 <2211403@iub.edu.bd>
2025-04-11 16:02:32 +0600

    show Orde and update status

---
+++ src/main/java/com/example/oop_final_project/Masum/ShowCustomerOrdersAndEditOrderStatusController.java
@@ -0,0 +1,57 @@
+package com.example.oop_final_project.Masum;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+public class ShowCustomerOrdersAndEditOrderStatusController {
+    @javafx.fxml.FXML
+    private TableColumn fishNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn customerLocationColumn;
+    @javafx.fxml.FXML
+    private Label showPricePerKgLabel;
+    @javafx.fxml.FXML
+    private TableColumn showCustomerLocationColumn;
+    @javafx.fxml.FXML
+    private TableColumn customerPhoneColumn;
+    @javafx.fxml.FXML
+    private TableColumn fishAmountColumn;
+    @javafx.fxml.FXML
+    private TableColumn showPricePerKgColumn;
+    @javafx.fxml.FXML
+    private TableColumn updateStutasCoumn;
+    @javafx.fxml.FXML
+    private TableColumn pricePerKgColumn;
+    @javafx.fxml.FXML
+    private TableColumn showCustomerPhoneColumn;
+    @javafx.fxml.FXML
+    private TableView OrderTableView;
+    @javafx.fxml.FXML
+    private TableColumn showFishAmountColumn;
+    @javafx.fxml.FXML
+    private TableColumn customerNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TableView updateOrderStutusTableView;
+    @javafx.fxml.FXML
+    private Label updateStatusErrorLabel;
+    @javafx.fxml.FXML
+    private Label showFishNameTF;
+    @javafx.fxml.FXML
+    private TableColumn totalCostCoumn;
+    @javafx.fxml.FXML
+    private TableColumn showTotalCostCoumn;
+    @javafx.fxml.FXML
+    private TableColumn showFishNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn showCustomerNameColumn;
+
+    @javafx.fxml.FXML
+    public void checkAvaibilityOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateStatusOnAction(ActionEvent actionEvent) {
+    }
+}
