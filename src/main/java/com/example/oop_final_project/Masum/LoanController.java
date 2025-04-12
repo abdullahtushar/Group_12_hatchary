dadbe1d9fc0e3f024577fd17fb338b34d93dde1a
masum179 <2211403@iub.edu.bd>
2025-04-12 18:04:32 +0600

    add Loan fxml and LoanController

---
+++ src/main/java/com/example/oop_final_project/Masum/LoanController.java
@@ -0,0 +1,30 @@
+package com.example.oop_final_project.Masum;
+
+import javafx.scene.control.*;
+
+public class LoanController {
+    @javafx.fxml.FXML
+    private TextField loanAmountTF;
+    @javafx.fxml.FXML
+    private DatePicker ApplyDateDP;
+    @javafx.fxml.FXML
+    private TableView loanTableView;
+    @javafx.fxml.FXML
+    private DatePicker returnDateDP;
+    @javafx.fxml.FXML
+    private TableColumn loanPurposeColumn;
+    @javafx.fxml.FXML
+    private TableColumn statusColumn;
+    @javafx.fxml.FXML
+    private TextField loanPurposeTF;
+    @javafx.fxml.FXML
+    private TableColumn loanAmountColumn;
+    @javafx.fxml.FXML
+    private TableColumn loanApplyDateColumn;
+    @javafx.fxml.FXML
+    private TableColumn loanReturnDateColumn;
+    @javafx.fxml.FXML
+    private Label errorLabel;
+    @javafx.fxml.FXML
+    private TableColumn loanIdColumn;
+}
