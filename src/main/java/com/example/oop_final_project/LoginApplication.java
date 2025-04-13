e626f9b52bcafe5b8082c85e6b788191faad9b78
masum179 <2211403@iub.edu.bd>
2025-04-14 02:01:18 +0600

    add LoginApplication

--- src/main/java/com/example/oop_final_project/LoginApplication.java
+++ src/main/java/com/example/oop_final_project/LoginApplication.java
@@ -16,7 +16,7 @@ public class LoginApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+//
     public static void main(String[] args) {
         launch();
     }
