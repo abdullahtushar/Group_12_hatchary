6fafeadcb1e86d048d0921a49b47a49e2f65fb75
masum179 <2211403@iub.edu.bd>
2025-04-14 17:05:34 +0600

    complete daily transaction

---
+++ src/main/java/com/example/oop_final_project/Masum/DailyTransaction.java
@@ -0,0 +1,87 @@
+package com.example.oop_final_project.Masum;
+
+import java.time.LocalDate;
+
+public class DailyTransaction {
+    private String transactionId, type,sourceOrPurpose;
+    private double amount;
+    private String notes;
+    private LocalDate date;
+
+    public DailyTransaction(String transactionId, String type, String sourceOrPurpose, double amount, String notes, LocalDate date) {
+        this.transactionId = transactionId;
+        this.type = type;
+        this.sourceOrPurpose = sourceOrPurpose;
+        this.amount = amount;
+        this.notes = notes;
+        this.date = date;
+    }
+
+    public DailyTransaction(String type, String sourceOrPurpose, double amount, String notes, LocalDate date) {
+        this.type = type;
+        this.sourceOrPurpose = sourceOrPurpose;
+        this.amount = amount;
+        this.notes = notes;
+        this.date = date;
+    }
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSourceOrPurpose() {
+        return sourceOrPurpose;
+    }
+
+    public void setSourceOrPurpose(String sourceOrPurpose) {
+        this.sourceOrPurpose = sourceOrPurpose;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "DailyTransaction{" +
+                "transactionId='" + transactionId + '\'' +
+                ", type='" + type + '\'' +
+                ", sourceOrPurpose='" + sourceOrPurpose + '\'' +
+                ", amount=" + amount +
+                ", notes='" + notes + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
--- src/main/java/com/example/oop_final_project/Masum/DailyTransactionController.java
+++ src/main/java/com/example/oop_final_project/Masum/DailyTransactionController.java
@@ -1,12 +1,22 @@
 package com.example.oop_final_project.Masum;
 
+import com.example.oop_final_project.LoginApplication;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.text.Text;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class DailyTransactionController {
     @javafx.fxml.FXML
-    private TableColumn sourceOrPurposeColumn;
+    private TableColumn<DailyTransaction,String> sourceOrPurposeColumn;
     @javafx.fxml.FXML
     private Label errorLabe;
     @javafx.fxml.FXML
@@ -14,31 +24,67 @@ public class DailyTransactionController {
     @javafx.fxml.FXML
     private TextField noteTF;
     @javafx.fxml.FXML
-    private TableColumn incomeOrExpanceColumn;
+    private TableColumn<DailyTransaction,String> incomeOrExpanceColumn;
     @javafx.fxml.FXML
-    private TableColumn noteColumn;
+    private TableColumn<DailyTransaction,String> noteColumn;
     @javafx.fxml.FXML
     private DatePicker addDP;
     @javafx.fxml.FXML
-    private TableColumn amountColumn;
+    private TableColumn<DailyTransaction,Integer> amountColumn;
     @javafx.fxml.FXML
-    private TableColumn dateColumn;
+    private TableColumn<DailyTransaction, LocalDate> dateColumn;
     @javafx.fxml.FXML
     private TextField amountTF;
     @javafx.fxml.FXML
-    private ComboBox incomeOrExpanceCB;
+    private ComboBox<String> incomeOrExpanceCB;
     @javafx.fxml.FXML
-    private TableView dailyTransactionTable;
-
+    private TableView<DailyTransaction> dailyTransactionTable;
+    public void initialize(){
+        incomeOrExpanceCB.getItems().addAll("Income","Expance");
+        incomeOrExpanceColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        sourceOrPurposeColumn.setCellValueFactory(new PropertyValueFactory<>("sourceOrPurpose"));
+        amountColumn.setCellValueFactory(new PropertyValueFactory<>("amount"));
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+        noteColumn.setCellValueFactory(new PropertyValueFactory<>("notes"));
+    }
+    ArrayList<DailyTransaction>dailyTransactionsList = new ArrayList<>();
     @javafx.fxml.FXML
-    public void backOnAction(ActionEvent actionEvent) {
+    public void backOnAction(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("FinanceOfficerDashboard.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
-    public void logoutOnAction(ActionEvent actionEvent) {
+    public void logoutOnAction(ActionEvent actionEvent) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
     @javafx.fxml.FXML
     public void addTransactionOnAction(ActionEvent actionEvent) {
+        if(incomeOrExpanceCB.getItems()==null){errorLabe.setText("Select type");return;}
+        if (amountTF.getText().isEmpty()){errorLabe.setText("Enter type");return;}
+        if (noteTF.getText().isEmpty()){errorLabe.setText("Enter note");return;}
+        if (addDP.getValue()==null){errorLabe.setText("Enter date");return;}
+        if (sourcePurposeTF.getText().isEmpty()){
+            errorLabe.setText("Enter Source or purpose");
+            return;
+        }
+        DailyTransaction dailyTransaction = new DailyTransaction(incomeOrExpanceCB.getValue(),
+                sourcePurposeTF.getText(),Double.parseDouble(amountTF.getText()),
+                noteTF.getText(),addDP.getValue());
+                dailyTransactionsList.add(dailyTransaction);
+                dailyTransactionTable.getItems().clear();
+               dailyTransactionTable.getItems().setAll(dailyTransactionsList);
+               errorLabe.setText("Transaction added successfully");
+
+
     }
 }
