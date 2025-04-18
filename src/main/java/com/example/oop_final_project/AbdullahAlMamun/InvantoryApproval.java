5a3b8ad41ec5746f70493d26603a2afaa94888f2
abdullahtushar <aamtushar09@gmail.com>
2025-04-18 10:20:19 +0600

    farm manager goals updated

---
+++ src/main/java/com/example/oop_final_project/AbdullahAlMamun/InvantoryApproval.java
@@ -0,0 +1,31 @@
+package com.example.oop_final_project.AbdullahAlMamun;
+
+public class InvantoryApproval
+{
+    @javafx.fxml.FXML
+    private TextField requstTextField;
+    @javafx.fxml.FXML
+    private TableColumn itemColumnIvantory;
+    @javafx.fxml.FXML
+    private TableView invantoryApprovalTable;
+    @javafx.fxml.FXML
+    private TableColumn quantityColumnInvantory;
+    @javafx.fxml.FXML
+    private TableColumn UserColumnInvantory;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void nextInvantoryOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ApproveButtonInvantory(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void prevousInvantoryOnAction(ActionEvent actionEvent) {
+    }
+}
