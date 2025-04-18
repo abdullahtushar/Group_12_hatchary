d00ce31d2b2afaa8ea7280b4e9b1ffc47bf35bc0
abdullahtushar <aamtushar09@gmail.com>
2025-04-18 12:18:17 +0600

    farm manager goals updated

---
+++ src/main/java/com/example/oop_final_project/AbdullahAlMamun/ExpansionPlan.java
@@ -0,0 +1,31 @@
+package com.example.oop_final_project.AbdullahAlMamun;
+
+public class ExpansionPlan
+{
+    @javafx.fxml.FXML
+    private TableView expansionTable;
+    @javafx.fxml.FXML
+    private TableColumn expansionPlanColumn;
+    @javafx.fxml.FXML
+    private DatePicker expansionDatePIck;
+    @javafx.fxml.FXML
+    private TableColumn expansionDatePick;
+    @javafx.fxml.FXML
+    private TextArea expansionTextArea;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void expansionHomeButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void saveExpansionButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void expansionPreviousButton(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/oop_final_project/AbdullahAlMamun/Maintenance.java
@@ -0,0 +1,35 @@
+package com.example.oop_final_project.AbdullahAlMamun;
+
+public class Maintenance
+{
+    @javafx.fxml.FXML
+    private TableColumn taskNameColumn;
+    @javafx.fxml.FXML
+    private TableColumn AssignedDateColumn;
+    @javafx.fxml.FXML
+    private TextField MantenanceTextField;
+    @javafx.fxml.FXML
+    private TableColumn AssignedUserColumn;
+    @javafx.fxml.FXML
+    private ComboBox AssignedUserCombo;
+    @javafx.fxml.FXML
+    private DatePicker assignedDatePicker;
+    @javafx.fxml.FXML
+    private TableView tableAssignMantenance;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void previousButtonAssign(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void nextbuttonMantenance(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void AssignedButton(ActionEvent actionEvent) {
+    }
+}
