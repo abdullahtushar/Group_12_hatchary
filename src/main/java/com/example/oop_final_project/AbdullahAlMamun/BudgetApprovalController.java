5eb94cb217ecf78355bfc4676f89eee29110e42e
abdullahtushar <aamtushar09@gmail.com>
2025-04-18 09:50:23 +0600

    farm manager tasks

---
+++ src/main/java/com/example/oop_final_project/AbdullahAlMamun/BudgetApprovalController.java
@@ -0,0 +1,10 @@
+package com.example.oop_final_project.AbdullahAlMamun;
+
+
+import javafx.event.ActionEvent;
+
+public class BudgetApprovalController {
+    @javafx.fxml.FXML
+    public void approveAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/com/example/oop_final_project/AbdullahAlMamun/FarmManagerGoals.java
@@ -0,0 +1,32 @@
+package com.example.oop_final_project.AbdullahAlMamun;
+
+public class FarmManagerGoals
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void previousFarmGoalsbutton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void maintenanceOnActiion(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void budgetButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void ExpansionPlanOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void inventoryButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void breadAndHatchOnAction(ActionEvent actionEvent) {
+    }
+}
