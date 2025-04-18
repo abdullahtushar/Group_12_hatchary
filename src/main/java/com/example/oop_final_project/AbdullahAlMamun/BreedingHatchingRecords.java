06d90fe90db4778409155f93e42fcccad3c0dc24
abdullahtushar <aamtushar09@gmail.com>
2025-04-18 12:35:56 +0600

    farm manager goals updated-breeding

---
+++ src/main/java/com/example/oop_final_project/AbdullahAlMamun/BreedingHatchingRecords.java
@@ -0,0 +1,31 @@
+package com.example.oop_final_project.AbdullahAlMamun;
+
+public class BreedingHatchingRecords
+{
+    @javafx.fxml.FXML
+    private TableView breedingTableVIew;
+    @javafx.fxml.FXML
+    private ComboBox breadingComboBox;
+    @javafx.fxml.FXML
+    private TableColumn breedingDateColumn;
+    @javafx.fxml.FXML
+    private DatePicker breadingDatePicker;
+    @javafx.fxml.FXML
+    private TableColumn BreedingSeletedFishColumn;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void breedingSubmitButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BreedingHome(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void BreedingPrevious(ActionEvent actionEvent) {
+    }
+}
